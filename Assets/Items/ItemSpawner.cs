3ef1f18fdd5e65cf52fa68bffbfca415a6ae3bad
Nick Marigo <nmarigo@ucsc.edu>
2026-05-02 14:00:57 -0700

    Added basic item spawning
    - Prefab Objects are added to the list in Inspector
    - Change const variables for spawning range

--- Assets/Items/ItemSpawner.cs
+++ Assets/Items/ItemSpawner.cs
@@ -2,5 +2,22 @@ using UnityEngine;
 
 public class ItemSpawner : MonoBehaviour {
     // ALL ITEM PREFABS HELD HERE
-    [SerializeField] GameObject[] items;
+    //[SerializeField] GameObject[] items;
+    [SerializeField] private List<GameObject> items = new List<GameObject>();
+
+    private const int spawnHeight = 10; //How High we want items to spawn
+    private const int xMin = -5;
+    private const int xMax = 5;
+    private const int zMin = -5;
+    private const int zMax = 5;
+
+    public void SpawnItem(int index)
+    {
+        Instantiate(items[index], MakeSpawnPosition(), Quaternion.identity);
+    }
+
+    private Vector3 MakeSpawnPosition()
+    {
+        return new Vector3(Random.Range(xMin, xMax), spawnHeight, Random.Range(zMin, zMax));
+    }
 }
