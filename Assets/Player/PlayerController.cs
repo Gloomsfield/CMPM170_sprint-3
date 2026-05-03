63299cb7604cead604ee89d310afa086d7d5add3
Max Kinet <max.a.kinet@gmail.com>
2026-05-03 13:42:45 -0700

    add raycasting from camera to mouse
    includes an addition of a new layer grabbable to determine what the raycast can hit

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
@@ -8,6 +9,25 @@ public class PlayerController : MonoBehaviour {
         WALKING,
         GRABBING,
         // ADD STATES HERE
+    }
+
+    private LayerMask grabbableMask;
+
+    [SerializeField] float grabRange;
+    
+    void Start() {
+        grabbableMask = LayerMask.GetMask("Grabbable");
+    }
+
+    void Update() {
+        // TODO Do we want to use both buttons? change to FixedUpdate?
+        if (Mouse.current.leftButton.wasPressedThisFrame) {
+            Vector2 mousePos = Mouse.current.position.ReadValue();
+            Ray ray = Camera.main.ScreenPointToRay(mousePos);
+            // TO REMOVE Show raycast in scene
+            Debug.DrawLine(ray.origin, ray.direction * grabRange, Color.red, 10f, false);
+            Debug.Log(Physics.Raycast(ray.origin, ray.direction, grabRange, grabbableMask));
+        }
     }
 
 
