1ba526981d3d8a73e2eaa569502b7a7e0c645a28
Nick Marigo <nmarigo@ucsc.edu>
2026-05-03 13:31:40 -0700

    Add player WASD controls

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,7 +1,10 @@
-using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+
+    private PlayerInputController playerInputController;
+
+    [SerializeField] private float speed;
 
     enum states {
         IDLE,
@@ -10,5 +13,16 @@ public class PlayerController : MonoBehaviour {
         // ADD STATES HERE
     }
 
+    private void Awake()
+    {
+        playerInputController = GetComponent<PlayerInputController>();
+    }
+
+    private void Update()
+    {
+        Vector3 positionChange = new Vector3(playerInputController.MovementInputVector.x, 0, playerInputController.MovementInputVector.y) * Time.deltaTime * speed;
+
+        transform.position += positionChange;
+    }
 
 }
