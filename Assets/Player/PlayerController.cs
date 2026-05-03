6522903cf7890f233446aedd664c58bd2d863b44
Nick Marigo <nmarigo@ucsc.edu>
2026-05-03 15:09:25 -0700

    Completely rework player movement (Still need to make comments)

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -2,27 +2,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    private PlayerInputController playerInputController;
-
-    [SerializeField] private float speed;
-
     enum states {
         IDLE,
         WALKING,
         GRABBING,
         // ADD STATES HERE
-    }
-
-    private void Awake()
-    {
-        playerInputController = GetComponent<PlayerInputController>();
-    }
-
-    private void Update()
-    {
-        Vector3 positionChange = new Vector3(playerInputController.MovementInputVector.x, 0, playerInputController.MovementInputVector.y) * Time.deltaTime * speed;
-
-        transform.position += positionChange;
     }
 
 }
