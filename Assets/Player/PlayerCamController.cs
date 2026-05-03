14138e6e02dddca860e66f9c60705a9826b8b50d
Max Kinet <max.a.kinet@gmail.com>
2026-05-02 22:24:35 -0700

    fix: incorporate x and y sens into playerCamController

--- Assets/Player/PlayerCamController.cs
+++ Assets/Player/PlayerCamController.cs
@@ -2,8 +2,8 @@ using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class PlayerCamController : MonoBehaviour {
-    [SerializeField] float xSens = 1f;
-    [SerializeField] float ySens = 1f;
+    [SerializeField] float xSens = 0.6f;
+    [SerializeField] float ySens = 0.6f;
 
     private float xRotation = 0;
     private float yRotation = 0;
@@ -16,8 +16,8 @@ public class PlayerCamController : MonoBehaviour {
     /* Moves the camera with the mouse, allowing the player to look around */
     void Update() {
         Vector2 rotationDelta = Mouse.current.delta.ReadValue();
-        xRotation -= rotationDelta.y;
-        yRotation += rotationDelta.x;
+        xRotation -= rotationDelta.y * ySens;
+        yRotation += rotationDelta.x * xSens;
         // Clamp so that we cannot look behind ourselves
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
