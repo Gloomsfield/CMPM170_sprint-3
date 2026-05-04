f136a062c9b0b567edf03599ab15c6bd9265d8e4
Max Kinet <max.a.kinet@gmail.com>
2026-05-03 21:35:37 -0700

    allow item to be let go even if raycast doesn't hit

--- Assets/Scripts/Items/GrabController.cs
+++ Assets/Scripts/Items/GrabController.cs
@@ -3,14 +3,26 @@ using UnityEngine;
 public class GrabController : MonoBehaviour, IsGrabbable
 {
     private bool grabbed = false;
+    private Rigidbody rb;
+    private FixedJoint grabJoint;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
 
     public void ToggleGrab() {
         grabbed = !grabbed;
         if (grabbed) {
+            rb.isKinematic = true;
+            rb.detectCollisions = false;  // Optional: turns off physics interaction while held
             transform.SetParent(Camera.main.transform);
+            /* TO REMOVE Create a joint, which attaches to rigidbodies
+            grabJoint = gameObject.AddComponent<FixedJoint>();
+            grabJoint.connectedBody = grabber;
+            grabJoint.breakForce = Mathf.Infinity;
+            */
         } else {
-            transform.SetParent(null);
+            rb.transform.SetParent(null);
         }
-
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,9 +12,11 @@ public class PlayerController : MonoBehaviour {
     }
 
     private LayerMask grabbableMask;
+    private bool grabbing = false;
+    private GrabController grabController;
 
     [SerializeField] float grabRange;
-    
+
     void Start() {
         grabbableMask = LayerMask.GetMask("Grabbable");
     }
@@ -25,15 +27,20 @@ public class PlayerController : MonoBehaviour {
          * be in the grabbable layer */
         // TODO Do we want to use both buttons? change to FixedUpdate?
         if (Mouse.current.leftButton.wasPressedThisFrame) {
-            Vector2 mousePos = Mouse.current.position.ReadValue();
-            Ray ray = Camera.main.ScreenPointToRay(mousePos);
-            RaycastHit hit;
-            // TO REMOVE Show raycast in scene
-            Debug.DrawLine(ray.origin, ray.direction * grabRange, Color.red, 10f, false);
-            if (Physics.Raycast(ray.origin, ray.direction, out hit, grabRange, grabbableMask)) {
-                GameObject target = hit.collider.gameObject;
-                target.GetComponent<GrabController>().ToggleGrab();
+            if (!grabbing) {
+                Vector2 mousePos = Mouse.current.position.ReadValue();
+                Ray ray = Camera.main.ScreenPointToRay(mousePos);
+                RaycastHit hit;
+                // TO REMOVE Show raycast in scene
+                Debug.DrawLine(ray.origin, ray.direction * grabRange, Color.red, 10f, false);
+                if (Physics.Raycast(ray.origin, ray.direction, out hit, grabRange, grabbableMask)) {
+                    grabbing = !grabbing;
+                    GameObject target = hit.collider.gameObject;
+                    grabController = target.GetComponent<GrabController>();
+                    grabController.ToggleGrab();
+                }
             }
+            grabController.ToggleGrab();
         }
     }
 }
