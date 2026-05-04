947bdf9fae0741894251a32ca3fb30b2551f7f18
Max Kinet <max.a.kinet@gmail.com>
2026-05-04 10:32:22 -0700

    pass camera head object instead of hard accessing main cam

--- Assets/Scripts/Items/GrabController.cs
+++ Assets/Scripts/Items/GrabController.cs
@@ -8,33 +8,35 @@ public class GrabController : MonoBehaviour, IsGrabbable
 
     private Vector3 lastCamPosition;
     private Vector3 camDelta;
+    private Camera grabberHead;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
-        lastCamPosition = Camera.main.transform.position;
     }
 
-    public void ToggleGrab() {
+    public void ToggleGrab(Camera head) {
         grabbed = !grabbed;
         if (grabbed) {
+            grabberHead = head;
+            lastCamPosition = grabberHead.transform.position;
             rb.isKinematic = true;
             //rb.transform.SetParent(Camera.main.transform);
-            rb.MovePosition(GetComponent<Camera>().transform.position);
             /* TO REMOVE Create a joint, which attaches to rigidbodies
             grabJoint = gameObject.AddComponent<FixedJoint>();
             grabJoint.connectedBody = grabber;
             grabJoint.breakForce = Mathf.Infinity;
             */
         } else {
-            rb.transform.SetParent(null);
+            grabberHead = null;
+            //rb.transform.SetParent(null);
         }
     }
 
     void Update() {
-        camDelta = Camera.main.transform.position - lastCamPosition;
         if (grabbed) {
+            camDelta = grabberHead.transform.position - lastCamPosition;
             rb.transform.position = rb.transform.position + camDelta;
+            lastCamPosition = grabberHead.transform.position;
         }
-        lastCamPosition = Camera.main.transform.position;
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -15,31 +15,43 @@ public class PlayerController : MonoBehaviour {
     private bool grabbing = false;
     private GrabController grabController;
 
+    [SerializeField] Camera head;
     [SerializeField] float grabRange;
 
     void Start() {
         grabbableMask = LayerMask.GetMask("Grabbable");
     }
 
+    /* Sends a raycast from the camera to grabRange on mouse click, calling ToggleGrab()
+     * on the hit object. IMPORTANT: grabbable objects must implement IsGrabbable and
+     * be in the grabbable layer */
+    void TryGrab() {
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        // TO REMOVE Show raycast in scene
+        Debug.DrawLine(ray.origin, ray.direction * grabRange, Color.red, 10f, false);
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, grabRange, grabbableMask)) {
+            grabbing = !grabbing;
+            GameObject target = hit.collider.gameObject;
+            Debug.Log("Grabbed " + target);
+            grabController = target.GetComponent<GrabController>();
+            grabController.ToggleGrab(head);
+            grabbing = true;
+        }
+    }
+
     void Update() {
-        /* Sends a raycast from the camera to grabRange on mouse click, calling ToggleGrab()
-         * on the hit object. IMPORTANT: grabbable objects must implement IsGrabbable and
-         * be in the grabbable layer */
         // TODO Do we want to use both buttons? change to FixedUpdate?
         if (Mouse.current.leftButton.wasPressedThisFrame) {
-            if (!grabbing) {
-                Vector2 mousePos = Mouse.current.position.ReadValue();
-                Ray ray = Camera.main.ScreenPointToRay(mousePos);
-                RaycastHit hit;
-                // TO REMOVE Show raycast in scene
-                Debug.DrawLine(ray.origin, ray.direction * grabRange, Color.red, 10f, false);
-                if (Physics.Raycast(ray.origin, ray.direction, out hit, grabRange, grabbableMask)) {
-                    grabbing = !grabbing;
-                    GameObject target = hit.collider.gameObject;
-                    grabController = target.GetComponent<GrabController>();
-                }
+            Debug.Log("press received");
+            if (grabbing) {
+                grabController.ToggleGrab(head);
+                grabbing = false;
+                Debug.Log("Dropped item");
+            } else {
+                TryGrab();
             }
-                grabController.ToggleGrab();
         }
     }
 }
