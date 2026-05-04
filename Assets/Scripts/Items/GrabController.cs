a3f9f67ce7990cacfb51d4c6678b63c1eaf16484
Max Kinet <max.a.kinet@gmail.com>
2026-05-04 11:30:50 -0700

    attach the object to camera via parenting

--- Assets/Scripts/Items/GrabController.cs
+++ Assets/Scripts/Items/GrabController.cs
@@ -6,8 +6,6 @@ public class GrabController : MonoBehaviour, IsGrabbable
     private Rigidbody rb;
     private FixedJoint grabJoint;
 
-    private Vector3 lastCamPosition;
-    private Vector3 camDelta;
     private Camera grabberHead;
 
     void Start() {
@@ -18,28 +16,12 @@ public class GrabController : MonoBehaviour, IsGrabbable
         grabbed = !grabbed;
         if (grabbed) {
             grabberHead = head;
-            lastCamPosition = grabberHead.transform.position;
             rb.isKinematic = true;
-            //rb.transform.SetParent(Camera.main.transform);
-            /* TO REMOVE Create a joint, which attaches to rigidbodies
-            grabJoint = gameObject.AddComponent<FixedJoint>();
-            grabJoint.connectedBody = grabber;
-            grabJoint.breakForce = Mathf.Infinity;
-            */
+            rb.transform.SetParent(grabberHead.transform);
         } else {
+            rb.transform.SetParent(null);
+            rb.isKinematic = false;
             grabberHead = null;
-            //rb.transform.SetParent(null);
-        }
-    }
-
-    void Update() {
-        if (grabbed) {
-            grabberHead.transform.position = grabberHead.transform.position + new Vector3 (0f, 0f, 0.01f);
-            camDelta = grabberHead.transform.position - lastCamPosition;
-            Debug.Log("cam " + camDelta);
-            rb.transform.position = rb.transform.position + camDelta;
-            Debug.Log("rb " + rb.transform.position);
-            lastCamPosition = grabberHead.transform.position;
         }
     }
 }
