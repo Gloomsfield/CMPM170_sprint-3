e04c72e53c4d7a6ed008ee3d73a82e289101c04b
Gloomsfield <amory@acosta.boo>
2026-05-05 21:48:28 -0700

    add patterns.json file

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager {
         /* EX This is how you suscribe a function to an event. You can use -= to unsuscribe.
          * Now, LogTherapyStartTime will get called whenever the therapyStarted event is triggered*/
         TherapyManager.therapyStarted += LogTherapyStartTime;
+
+		EvaluationHandler.Instance.MakePatternsFromJson(Resources.Load<TextAsset>("patterns").text);
     }
 
     public static GameManager Instance { get
