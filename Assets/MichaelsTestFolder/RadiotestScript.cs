af52de428d00730e332256c4a2192ee8d5cd2c9f
Michael Stager <61891711+MichaelStager@users.noreply.github.com>
2026-05-14 16:22:02 -0700

    Making a playscene with everything

--- Assets/MichaelsTestFolder/RadiotestScript.cs
+++ Assets/MichaelsTestFolder/RadiotestScript.cs
@@ -4,8 +4,6 @@ using UnityEngine.InputSystem;
 public class RadiotestScript : MonoBehaviour
 {
     [SerializeField] private AudioClip BGM;
-    [SerializeField] private AudioClip BGM2;
-    [SerializeField] private AudioClip crashSound;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,14 +11,6 @@ public class RadiotestScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(Keyboard.current.rKey.wasPressedThisFrame)
-        {
-            Debug.Log("R key was pressed, changing music.");
-            AudioManager.Instance.SetMusicVolume(.3f);
-            AudioManager.Instance.PlayMusic(BGM2,true);
-        }
-    }
+    
 
 }
