72216f96d3bd7739e0756c275d9f4a0de2639cc4
Gloomsfield <amory@acosta.boo>
2026-05-14 18:53:24 -0700

    add ResponseGenerator and TherapistState skeletons

---
+++ Assets/Scripts/Therapy/ResponseGenerator/ResponseGenerator.cs
@@ -0,0 +1,10 @@
+
+public class ResponseGenerator {
+
+	// TODO
+	public static string Generate(BehaviorLog log, TherapistState state) {
+		return "";
+	}
+
+}
+
---
+++ Assets/Scripts/Therapy/TherapistState/TherapistState.cs
@@ -0,0 +1,7 @@
+
+public class TherapistState {
+
+	public int aggressionLevel = 0;
+
+}
+
