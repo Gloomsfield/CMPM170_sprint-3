2a41a8b17ae9644593dca663c2c6d9354dad3f56
Nick Marigo <118312264+Nick-Marigo@users.noreply.github.com>
2026-05-15 00:43:00 -0700

    Added the most epic intro animation for play scene

---
+++ Assets/Scripts/Camera/IntroCameraControl.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using Unity.Cinemachine;
+using System.Collections;
+using UnityEngine.Playables;
+
+public class IntroCameraControl : MonoBehaviour
+{
+    [Header("Cinemachine Camera")]
+    [SerializeField] CinemachineCamera skyCamera;
+    [SerializeField] CinemachineCamera dollyCamera;
+    [SerializeField] CinemachineCamera playerCamera;
+
+    [Header("Timing")]
+    [SerializeField] float skyViewTime = 2f;
+    [SerializeField] float playerCamTime = 2f;
+
+    [Header("Timeline")]
+    [SerializeField] PlayableDirector dollyTimeLine;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(PlayIntro());
+    }
+
+    IEnumerator PlayIntro()
+    {
+        SetCamera(skyCamera);
+
+        yield return new WaitForSeconds(skyViewTime);
+
+        SetCamera(dollyCamera);
+        dollyTimeLine.Play();
+
+        yield return new WaitForSeconds((float)dollyTimeLine.duration);
+
+        SetCamera(playerCamera);
+
+        yield return new WaitForSeconds(playerCamTime);
+        EventManager.InvokeIntroSceneComplete();
+    }
+
+    void SetCamera(CinemachineCamera activeCamera)
+    {
+        skyCamera.Priority = 10;
+        dollyCamera.Priority = 10;
+        playerCamera.Priority = 10;
+
+        activeCamera.Priority = 30;
+    }
+}
